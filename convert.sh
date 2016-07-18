5f8e98574facb7ebb4a46ab052c59c11aba21303
Camil Staps <info@camilstaps.nl>
2016-07-18 22:26:14 +0200

    Resolves getbible/txtFactory#1

---
+++ convert.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Usage: convert.sh [-l LANGUAGE] [-d TEXTDIRECTION] -t TRANSLATION -a ABBREVIATION -i INFILE
+# Requirements: moreutils, grep, sed
+
+INFILE=
+LANGUAGE="English"
+TRANSLATION=
+ABBREVIATION=
+TEXTDIRECTION="LTR"
+
+while getopts l:t:a:d:i: opt
+do
+	case "$opt" in
+		l)
+			LANGUAGE="$OPTARG"
+			;;
+		t)
+			TRANSLATION="$(echo "$OPTARG" | sed 's/[^a-zA-Z]/_/g')"
+			;;
+		a)
+			ABBREVIATION="$OPTARG"
+			;;
+		d)
+			TEXTDIRECTION="$OPTARG"
+			;;
+		i)
+			INFILE="$OPTARG"
+			;;
+	esac
+done
+
+if [[ -z "$INFILE" ]] || [[ -z "$TRANSLATION" ]] || [[ -z "$ABBREVIATION" ]]
+then
+	echo "Usage: $0 [-l LANGUAGE] [-d TEXTDIRECTION] -t TRANSLATION -a ABBREVIATION -i INFILE"
+	exit -1
+fi
+
+OUTFILE="${LANGUAGE}__${TRANSLATION}__${ABBREVIATION}__${TEXTDIRECTION}.txt"
+
+rm -f "$OUTFILE"
+
+grep -v '^#' "$INFILE" | \
+	while read line
+	do
+		echo "$line" | sed 's/\t/||/g' >> "$OUTFILE"
+	done
+
+iconv -t UTF-8 "$OUTFILE" | sponge "$OUTFILE"
